e6aed1e58ad39d3e06eeb5c37cc9f679b089ff73
Mary Jonah <jonahmary17@gmail.com>
2024-11-12 19:20:53 +0000

    Frontend - Assign promises to requests in service file

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -3,27 +3,23 @@ import axios from 'axios'
 const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
-    return axios
-        .get(baseUrl)
-        .then(response => response.data)
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
 }
 
 const create = (newObject) => {
-    return axios 
-        .post(baseUrl, newObject)
-        .then(response => response.data)
+    const request = axios .post(baseUrl, newObject)
+    return request.then(response => response.data)
 }
 
 const deletePerson = person => {
-    return axios
-        .delete(`${baseUrl}/${person.id}`)
-        .then( response => response.data )
+    const request = axios.delete(`${baseUrl}/${person.id}`)
+    return request.then( response => response.data )
 }
 
 const update = (person) => {
-    return axios
-        .put(`${baseUrl}/${person.id}`, person)
-        .then(response => response.data)
+    const request = axios.put(`${baseUrl}/${person.id}`, person)
+    return request.then(response => response.data)
 }
 
 export default {getAll, create, deletePerson, update}
