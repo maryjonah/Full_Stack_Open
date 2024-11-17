f990fd28b6e5ed37c7c0f997139b8ad2c2525968
Mary Jonah <jonahmary17@gmail.com>
2024-11-17 17:08:26 +0000

    Backend Assignment - Refactor into main and sub describe blocks

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -9,111 +9,118 @@ const api = supertest(app)
 
 const Blog = require('../models/blog')
 
-beforeEach(async () => {
-    await Blog.deleteMany({})
+describe('when there are some initial blogs saved', () => {
 
-    const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
-    const promiseArray = blogObjects.map(blog => blog.save())
-    await Promise.all(promiseArray)
-})
-
-test('blogs are returned as json', async () => {
-    await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
-})
-
-test('there are 2 notes initially saved automatically for each test', async () => {
-    const response = await api.get('/api/blogs')
-    assert.strictEqual(response.body.length, helper.initialBlogs.length)
-})
-
-test('the default property __id is renamed to id', async () => {
-    const response = await api.get('/api/blogs')
-    const firstBlog = response.body[0]
-    assert.ok(firstBlog.hasOwnProperty('id'))
-})
-
-test('a valid blog can be added and the total number of blogs increases by 1', async () => {
-    const newBlog = {
-        title: "The gods are not be blamed",
-        author: "Oluwatosin",
-        url: "https://www.oreilly.com",
-        likes: 500
-    }
+    beforeEach(async () => {
+        await Blog.deleteMany({})
     
-    await api.post('/api/blogs').send(newBlog).expect(201)
-
-    const currentBlogs = await helper.blogsInDb()
-    assert.strictEqual(currentBlogs.length, helper.initialBlogs.length + 1)
-
-    const blogTitles = currentBlogs.map(blog => blog.title)
-    assert(blogTitles.includes('The gods are not be blamed'))
-})
-
-test('a valid blog with no likes defaults to 0 likes', async () => {
-    const newBlog = {
-        title: "The gods are not be blamed",
-        author: "Oluwatosin",
-        url: "https://www.oreilly.com",
-    }
-    
-    await api.post('/api/blogs').send(newBlog).expect(201)
-
-    const currentBlogs = await helper.blogsInDb()
-    const lastBlog = currentBlogs[currentBlogs.length - 1]
-
-    assert.equal(lastBlog.likes, 0)
-})
-
-test('a new blog without title is not saved', async () => {
-    const newBlog = {
-        author: "Galvin Meld",
-        url: "https://oreilly.com",
-        likes: 10
-    }
-    await api.post('/api/post').send(newBlog)
-    
-    const currentBlogs = await helper.blogsInDb()
-    assert.strictEqual(currentBlogs.length, helper.initialBlogs.length)
-})
-
-test('a new blog without url is not saved', async () => {
-    const newBlog = {
-        title: "My first copy book",
-        author: "Galvin Meld",
-        likes: 10000
-    }
-    await api.post('/api/post').send(newBlog)
-    
-    const currentBlogs = await helper.blogsInDb()
-    assert.strictEqual(currentBlogs.length, helper.initialBlogs.length)
-})
-
-describe('deletion of a blog', () => {
-    test('succeeds with 204 when id is valid', async () => {
-        const blogsAtStart = await helper.blogsInDb()
-        const blogToDelete = blogsAtStart[0]
-
-        await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
-
-        const blogsAtEnd = await helper.blogsInDb()
-        assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+        const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+        const promiseArray = blogObjects.map(blog => blog.save())
+        await Promise.all(promiseArray)
     })
-})
-
-describe('updating the likes of a blog', () => {
-    test('likes of a blog is updated', async () => {
-        const blogsAtStart = await helper.blogsInDb()
-        const firstBlog = blogsAtStart[0]
-        firstBlog.likes = 10
-
-        await api.put(`/api/blogs/${firstBlog.id}`).send(firstBlog).expect(200)
-
-        const blogsAtEnd = await helper.blogsInDb()
-        const updatedBlog = blogsAtEnd.filter(blog => blog.id === `${firstBlog.id}`)
+    
+    test('blogs are returned as json', async () => {
+        await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
+    })
+    
+    test('there are 2 notes initially saved automatically for each test', async () => {
+        const response = await api.get('/api/blogs')
+        assert.strictEqual(response.body.length, helper.initialBlogs.length)
+    })
+    
+    test('the default property __id is renamed to id', async () => {
+        const response = await api.get('/api/blogs')
+        const firstBlog = response.body[0]
+        assert.ok(firstBlog.hasOwnProperty('id'))
+    })
+    
+    describe('addition of a blog post', () => {
+        test('succeeds with valid data and number of blogs increases by 1', async () => {
+            const newBlog = {
+                title: "The gods are not be blamed",
+                author: "Oluwatosin",
+                url: "https://www.oreilly.com",
+                likes: 500
+            }
+            
+            await api.post('/api/blogs').send(newBlog).expect(201)
         
-        assert.equal(updatedBlog[0].likes, 10)
+            const currentBlogs = await helper.blogsInDb()
+            assert.strictEqual(currentBlogs.length, helper.initialBlogs.length + 1)
+        
+            const blogTitles = currentBlogs.map(blog => blog.title)
+            assert(blogTitles.includes('The gods are not be blamed'))
+        })
+        
+        test('succeeds with 201 and likes 0 when data does not have likes data', async () => {
+            const newBlog = {
+                title: "The gods are not be blamed",
+                author: "Oluwatosin",
+                url: "https://www.oreilly.com",
+            }
+            
+            await api.post('/api/blogs').send(newBlog).expect(201)
+        
+            const currentBlogs = await helper.blogsInDb()
+            const lastBlog = currentBlogs[currentBlogs.length - 1]
+        
+            assert.equal(lastBlog.likes, 0)
+        })
+        
+        test('number of posts is not increased when data does nt include title', async () => {
+            const newBlog = {
+                author: "Galvin Meld",
+                url: "https://oreilly.com",
+                likes: 10
+            }
+            await api.post('/api/post').send(newBlog)
+            
+            const currentBlogs = await helper.blogsInDb()
+            assert.strictEqual(currentBlogs.length, helper.initialBlogs.length)
+        })
+        
+        test('number of posts is not increased when data does not include url', async () => {
+            const newBlog = {
+                title: "My first copy book",
+                author: "Galvin Meld",
+                likes: 10000
+            }
+            await api.post('/api/post').send(newBlog)
+            
+            const currentBlogs = await helper.blogsInDb()
+            assert.strictEqual(currentBlogs.length, helper.initialBlogs.length)
+        })    
     })
+    
+    describe('deletion of a blog', () => {
+        test('succeeds with 204 when id is valid', async () => {
+            const blogsAtStart = await helper.blogsInDb()
+            const blogToDelete = blogsAtStart[0]
+    
+            await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+    
+            const blogsAtEnd = await helper.blogsInDb()
+            assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+        })
+    })
+    
+    describe('updating the likes of a blog', () => {
+        test('likes of a blog is updated', async () => {
+            const blogsAtStart = await helper.blogsInDb()
+            const firstBlog = blogsAtStart[0]
+            firstBlog.likes = 10
+    
+            await api.put(`/api/blogs/${firstBlog.id}`).send(firstBlog).expect(200)
+    
+            const blogsAtEnd = await helper.blogsInDb()
+            const updatedBlog = blogsAtEnd.filter(blog => blog.id === `${firstBlog.id}`)
+            
+            assert.equal(updatedBlog[0].likes, 10)
+        })
+    })
+    
 })
+
 
 after(async () => {
     await mongoose.connection.close()
