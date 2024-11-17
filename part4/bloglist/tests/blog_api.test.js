a4f79651621271589446e913111bf2df43f78c0e
Mary Jonah <jonahmary17@gmail.com>
2024-11-17 16:32:17 +0000

    Backend Assignment - 4.13 Implement deletion of a blog and include test

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach, expect } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -86,6 +86,19 @@ test('a new blog without url is not saved', async () => {
     
     const currentBlogs = await helper.blogsInDb()
     assert.strictEqual(currentBlogs.length, helper.initialBlogs.length)
+})
+
+describe('deletion of a blog', () => {
+
+    test('succeeds with 204 when id is valid', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const blogToDelete = blogsAtStart[0]
+
+        await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+        const blogsAtEnd = await helper.blogsInDb()
+        assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+    })
 })
 
 after(async () => {
