25122f15d997956206f33b2828d406e3022fc89b
Mary Jonah <jonahmary17@gmail.com>
2024-11-23 11:15:42 +0000

    Frontend 4a Creating new notes

--- src/App.jsx
+++ src/App.jsx
@@ -67,6 +67,8 @@ const App = () => {
     
     try {
       const user = await loginService.login({ username, password })
+      noteService.setToken(user.token)
+      
       setUser(user)
       setUsername('')
       setPassword('')
