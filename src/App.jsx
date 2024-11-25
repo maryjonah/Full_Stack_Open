d9562e055743ad8a2991f891349d95ff432fdaff
Mary Jonah <jonahmary17@gmail.com>
2024-11-25 05:25:01 +0000

    Frontend 5b Displaying the login form only when appropraite

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { useState, useEffect } from 'react'
 import Note from './components/Note'
 import Notification from './components/Notification'
 import Footer from './components/Footer'
+import LoginForm from './components/LoginForm'
 
 import noteService from './services/notes'
 import loginService from './services/login'
 
 const App = () => {
+  const [loginVisible, setLoginVisible] = useState(false)
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(false)
@@ -109,18 +111,29 @@ const App = () => {
     ? notes
     : notes.filter(note => note.important)
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
+  const loginForm = () => {
+    const hideWhenVisible = { display: loginVisible ? 'none' : '' }
+    const showWhenVisible = { display: loginVisible ? '' : 'none'}
+
+    return (
       <div>
-        username <input type="text" value={username} name="Username" onChange={handleUsernameChange} />
+        <div style={ hideWhenVisible }>
+          <button onClick={ () => setLoginVisible(true)}>log in</button>
+        </div>
+        
+        <div style={ showWhenVisible}>
+          <LoginForm
+            username={ username }
+            password = { password }
+            handleUsernameChange={ handleUsernameChange }
+            handlePasswordChange={ handlePasswordChange }
+            handleSubmit = { handleLogin }
+          />
+          <button onClick={ () => setLoginVisible(false) }>cancel</button>
+        </div>
       </div>
-      <div>
-        password <input type="password" value={password} name="Password" onChange={handlePasswordChange} />
-      </div>
-      
-      <button type="submit">login</button>
-    </form>
-  )
+    )
+  }
 
   const noteForm = () => (
     <form onSubmit={addNote}>
