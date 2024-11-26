7613b22451a029e9721eb5da0eb8cb43ed65a58a
Mary Jonah <jonahmary17@gmail.com>
2024-11-26 22:48:05 +0000

    Frontend Exercise 5.7 a Make show/hide button and add styles to each blog post

--- part4/bloglist-frontend/src/components/Blog.jsx
+++ part4/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,27 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showFullDetails, setShowFullDetails] = useState(false)
+
+  const hideWhenVisible = { display: showFullDetails ? 'none' : '' }
+  const showWhenVisible = { display: showFullDetails ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+  <div style={blogStyle }>
+    <p>{blog.title}<button onClick={() => setShowFullDetails(!showFullDetails)}>{showFullDetails ? 'show' : 'hide '}</button></p>
+    <p>{blog.url}</p>
+    <p>likes {blog.likes}</p>
+    <p>{blog.author}</p>
   </div>  
-)
+  )
+}
 
 export default Blog
