f95ca1e8ec877a5d815f6e3920ca27cb3e92700b
Mary Jonah <jonahmary17@gmail.com>
2024-11-30 08:08:35 +0000

    Frontend 5c Rendering the component for tests

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'eslint-plugin-vitest-globals'
 
 export default [
   { ignores: ['dist'] },
@@ -22,6 +23,7 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'globals': globals
     },
     rules: {
       ...js.configs.recommended.rules,
