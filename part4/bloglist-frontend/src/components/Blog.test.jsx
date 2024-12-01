af96a915782e2f74a8175c19bda26d7b98d3f668
Mary Jonah <jonahmary17@gmail.com>
2024-12-01 16:34:03 +0000

    Frontend Exercise 5.15 Format according to eslint suggestions

--- part4/bloglist-frontend/src/components/Blog.test.jsx
+++ part4/bloglist-frontend/src/components/Blog.test.jsx
@@ -3,38 +3,37 @@ import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
-    let container
+  let container
 
-    beforeEach(() => {
-        const blog = {
-            title: 'Pig Years',
-            author: 'Ellen Gaydos',
-            url: 'https://www.npr.org/2024/11/29/1215793964/nprs-book-of-the-day-pig-years-what-the-chicken-knows',
-            user: {
-              username: 'ekuajonah'
-            }
-        }
+  beforeEach(() => {
+    const blog = {
+      title: 'Pig Years',
+      author: 'Ellen Gaydos',
+      url: 'https://www.npr.org/2024/11/29/1215793964/nprs-book-of-the-day-pig-years-what-the-chicken-knows',
+      user: {
+        username: 'ekuajonah'
+      }
+    }
 
-        container = render(<Blog blog={ blog } />).container
-    })
+    container = render(<Blog blog={ blog } />).container
+  })
 
-    test('at the start only the title of the Blog can be seen, author details hidden until view button clicked', () => {
-        // Check that title is visible
-        const titleElement = screen.getByText('Pig Years')
-        expect(titleElement).toBeDefined()
-      
-        // Check that author details are initially hidden
-        const otherTextDiv = container.querySelector('.otherBlogInfo')
-        expect(otherTextDiv).toHaveStyle('display: none')
-      
-    })
+  test('at the start only the title of the Blog can be seen, author details hidden until view button clicked', () => {
+    // Check that title is visible
+    const titleElement = screen.getByText('Pig Years')
+    expect(titleElement).toBeDefined()
 
-    test('when view button is clicked, blog author, likes, and url is displayed', async () => {
-        const user = userEvent.setup()
-        const viewBtn = screen.getByText('view')
-        await user.click(viewBtn)
+    // Check that author details are initially hidden
+    const otherTextDiv = container.querySelector('.otherBlogInfo')
+    expect(otherTextDiv).toHaveStyle('display: none')
+  })
 
-        const otherBlogDetails = container.querySelector('.otherBlogInfo')
-        expect(otherBlogDetails).not.toHaveStyle('display: none')
-    })
+  test('when view button is clicked, blog author, likes, and url is displayed', async () => {
+    const user = userEvent.setup()
+    const viewBtn = screen.getByText('view')
+    await user.click(viewBtn)
+
+    const otherBlogDetails = container.querySelector('.otherBlogInfo')
+    expect(otherBlogDetails).not.toHaveStyle('display: none')
+  })
 })
