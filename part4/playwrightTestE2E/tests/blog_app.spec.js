017382f6160761f8355e170630be7b529e30c141
Mary Jonah <jonahmary17@gmail.com>
2024-12-04 14:35:44 +0000

    Playwright Assignment - 5.18 Test successful and failed login

--- part4/playwrightTestE2E/tests/blog_app.spec.js
+++ part4/playwrightTestE2E/tests/blog_app.spec.js
@@ -1,12 +1,32 @@
 const  { beforeEach, describe, expect, test } = require('@playwright/test')
+const { loginWith } = require('./blog_helper')
 
 describe('Blog app', () => {
-    beforeEach(async ({ page }) => {
-        await page.goto('http://localhost:5173')
+    beforeEach(async ({ page, request }) => {
+        await request.post('/api/testing/reset')
+        await request.post('/api/users', {
+            data: {
+                name: 'Ekua Kakra Jonah',
+                username: 'kajonah',
+                password: 'HelloWorld'
+            }
+        })
+        await page.goto('/')
     })
 
     test('Login form is shown', async ({ page }) => {
         const locator = page.getByText('log in to application')
         await expect(locator).toBeVisible()
+    })
+
+    describe('Login', () => {
+        test('succeeds with correct credentials', async ({ page }) => {
+            await loginWith(page, 'kajonah', 'HelloWorld')
+            await expect(page.getByText('Ekua Kakra Jonah logged')).toBeVisible()
+        })
+        test('fails with wrong credentials', async ({ page }) => {
+            await loginWith(page, 'mjay', 'World')
+            await expect(page.getByText('Ekua Kakra Jonah')).not.toBeVisible()
+        })
     })
 })
